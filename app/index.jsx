b16118e6cb2dd22a62d521c78dd6973f4b88b603
Dustin Keppler <dtkeppler@gmail.com>
2016-09-08 07:40:20 -0400

    added user object array

--- app/index.jsx
+++ app/index.jsx
@@ -3,12 +3,48 @@ import ReactDOM from 'react-dom';
 
 class MessageInput extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      messages : [
+        {
+          text: "Bacon ipsum dolor amet swine boudin salami pork chop tenderloin ham hock rump frankfurter, jerky alcatra.",
+          time: "1:00 pm",
+          user: "Dallas"
+        },
+        {
+          text: "Strip steak chicken swine turkey shankle prosciutto. Kevin prosciutto beef kielbasa tongue pork.",
+          time: "2:00 pm",
+          user: "Wendell"
+        },
+        {
+          text: "Sirloin frankfurter ball tip, cupim pig hamburger pork belly meatball strip steak corned beef.",
+          time: "3:00 pm",
+          user: "Duke"
+        },
+        {
+          text: "Pastrami ribeye frankfurter cupim tenderloin biltong shoulder kevin pork loin corned beef rump salami cow andouille shankle.",
+          time: "4:00 pm",
+          user: "Toki"
+        }
+    ]}
+  }
+
   render() {
+
+    const messageMap = this.state.messages.map(function(user, i) {
+      for ( i = 0; i < this.state.length; i++) {
+        return (<li key={i}>At {user.time}, {user.user} said "{user.text}"</li>)
+      }
+    })
+
     return (
       <div>
-        <p>Welcome to Dustin and Kevins page</p>
-        <label>Enter text here<input type="text" /></label>
-        <input type="submit" value="Submit" />
+
+        <ul>{messageMap}</ul>
+        // <p>Welcome to Dustin and Kevins page</p>
+        // <label>Enter text here<input type="text" /></label>
+        // <input type="submit" value="Submit" />
       </div>
     )
   }
